daf5c8731804ed30d1e1335d5fab80bc377fb04e
Shafiullah Shafin <shafiullahshafin735@gmail.com>
2026-01-06 19:53:25 +0600

    Added API Running Meesage in access_control/urls.py

--- access_control/urls.py
+++ access_control/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.http import HttpResponse
 from .views import AccessLogListCreateView, AccessLogDetailView
 
 urlpatterns = [
+    path('', lambda r: HttpResponse("Access Control API Running")),
     path('api/logs/', AccessLogListCreateView.as_view(), name='accesslog-list-create'),
     path('api/logs/<int:pk>/', AccessLogDetailView.as_view(), name='accesslog-detail'),
 ]
