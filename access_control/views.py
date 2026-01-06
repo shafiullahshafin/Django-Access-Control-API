0b2a4b120dea2fdbd4120cc92843da772edd20c8
Shafiullah Shafin <shafiullahshafin735@gmail.com>
2026-01-06 18:37:41 +0600

    Added filtering capability to list endpoint

--- access_control/views.py
+++ access_control/views.py
@@ -1,15 +1,25 @@
 from rest_framework import generics
+from django_filters import rest_framework as filters
 from .models import AccessLog
 from .serializers import AccessLogSerializer
+
+class AccessLogFilter(filters.FilterSet):
+    card_id = filters.CharFilter(lookup_expr='iexact')
+    door_name = filters.CharFilter(lookup_expr='icontains')
+    access_granted = filters.BooleanFilter()
+    
+    class Meta:
+        model = AccessLog
+        fields = ['card_id', 'door_name', 'access_granted']
 
 class AccessLogListCreateView(generics.ListCreateAPIView):
     queryset = AccessLog.objects.all()
     serializer_class = AccessLogSerializer
+    filterset_class = AccessLogFilter
 
 class AccessLogDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = AccessLog.objects.all()
     serializer_class = AccessLogSerializer
     
     def perform_update(self, serializer):
-        # Prevent timestamp updates
         serializer.save()
